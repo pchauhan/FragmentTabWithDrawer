81b992ca0d9aa3c42bb4dcc52a3d53eb13fe3e12
Parag Chauhan <parag.c@planpixels.com>
2018-12-17 20:30:08 +0530

    changed gradle

--- app/src/main/java/com/sn/fragmenttabwithdrawer/TabB/AppTabBFirstFragment.java
+++ app/src/main/java/com/sn/fragmenttabwithdrawer/TabB/AppTabBFirstFragment.java
@@ -16,13 +16,13 @@ import com.sn.fragmenttabwithdrawer.constant.BaseFragment;
 public class AppTabBFirstFragment extends BaseFragment {
 
     private Button mGotoButton;
-
+    int counter ;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) {
         View view       =   inflater.inflate(R.layout.app_tab_b_first_screen, container, false);
         TextView tv = (TextView)view.findViewById(R.id.tv);
-        tv.setText("Tab B  \nFirst Fragment");
+        tv.setText("Tab B  \nFirst Fragment counter"+String.valueOf(counter));
         mGotoButton =   (Button) view.findViewById(R.id.id_next_tab_b_button);
         mGotoButton.setOnClickListener(listener);
 
@@ -33,6 +33,7 @@ public class AppTabBFirstFragment extends BaseFragment {
         @Override
         public void onClick(View v){
             /* Go to next fragment in navigation stack*/
+            counter = counter+1;
             mActivity.pushFragments(AppConstants.TAB_B, new AppTabBSecondFragment(),true,true);
         }
     };
